b0fd487111bd3f7bde780f0a2391a41fddbcc469
Aviral Varshney <aviralvarshney07@gmail.com>
2026-01-11 19:27:12 +0530

    Basics

---
+++ Basics/class-component/src/components/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+class Home extends React.Component{
+    title = "Meow";
+    showMessage = () =>{
+        return <h1>Haule Haule se dua lgti hai</h1>
+    }
+    render(){
+        return(
+            <>
+                <h2>Hello World</h2>
+                <h1>{this.showMessage()}</h1>
+                <h3>{this.title}</h3>
+            </>
+        );
+    }
+}
+export default Home;
