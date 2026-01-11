ade1865fc735381c0e509ce815003909b1ff4df1
Aviral Varshney <aviralvarshney07@gmail.com>
2026-01-11 20:20:20 +0530

    Props in functional

--- Basics/functional-component/src/App.jsx
+++ Basics/functional-component/src/App.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import Home from "./components/Home";
-
+import Props from "./components/Props"
 const App = ()=>{
   return(
+    <>
     <Home />
+    <Props name="Aviral" age={20}/>
+    <Props name="Ved" age={55}/>
+    </>
   )
 }
-export default Home;
+export default App;
---
+++ Basics/functional-component/src/components/Props.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Props(props){
+    return(
+        <>
+
+        <h2>Hello {props.name}</h2>
+        <h2>Aged {props.age}</h2>
+        
+        </>
+    );
+}
+export default Props;
