52a1fae1bf9c39729fbb9f9d86b5dc43854698d9
Aviral Varshney <aviralvarshney07@gmail.com>
2026-01-13 11:07:07 +0530

    Styling

--- hooks/multipleContext/src/components/Profile.jsx
+++ hooks/multipleContext/src/components/Profile.jsx
@@ -1,32 +1,28 @@
-import React, {useState} from "react";
+import React, {useContext} from "react";
 import UserContext from "./UserContext";
 import ThemeContext from "./ThemeContext";
-import { useContext } from "react";
 
 function Profile(){
     const {user, setUser} = useContext(UserContext);
     const {theme, setTheme} = useContext(ThemeContext);
-    const pageStyle = {
-        backgroundColor: theme === "light" ? "#ffffff" : "#1e1e1e",
-        color: theme === "light" ? "#000000" : "#ffffff",
+
+    const style = {
+        backgroundColor: theme==="light"?"#ffffff":"#1e1e1e",
+        color: theme==="light"?"#000000":"#ffffff"
     };
+    const handleChange = (e)=>{
+        setUser(e.target.value);
+    }
+    const handleClick = () =>{
+        setTheme(theme==="light"?"dark":"light");
+    }
     return(
         <>
-       <div style={pageStyle}>
-            <h1>Name: {user}</h1>
-            <h3>Current Theme: {theme}</h3>
-
-            <input
-              type="text"
-              placeholder="Enter name"
-              onChange={(e) => setUser(e.target.value)}
-            />
-
-            <br /><br />
-
-            <button style = {pageStyle} onClick={() => setTheme(theme === "light" ? "dark" : "light")}>
-              Toggle Theme
-            </button>
+        <div style={style}>
+        <input type="text" placeholder="Enter name: " onChange={handleChange} value={user}/>
+        <br />
+        <br />
+        <button style = {style} onClick={handleClick}>Toggle</button>
         </div>
         </>
     )
