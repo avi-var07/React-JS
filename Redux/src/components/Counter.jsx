da4f02f8d523041e7a9c47ef3448ecc1b0d60d6e
Aviral Varshney <aviralvarshney07@gmail.com>
2026-01-14 19:36:44 +0530

    Chnages

---
+++ Redux/src/components/Counter.jsx
@@ -0,0 +1,16 @@
+import {useSelector, useDispatch} from "react-redux";
+import {increment, decrement} from "../counterSlice";
+
+function Counter(){
+    const count = useSelector((state)=>state.counter.count);
+    const dispatch = useDispatch();
+
+    return(
+        <>
+        <h1>Count: {count}</h1>
+        <button onClick={()=>dispatch(increment())}>+</button>
+        <button onClick={()=>dispatch(decrement())}>-</button>
+        </>
+    );
+}
+export default Counter;
